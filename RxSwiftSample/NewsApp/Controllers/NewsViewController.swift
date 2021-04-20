42397baf6ccbbe665c93d1a56cabe01beec68984
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-04-20 15:44:31 +0900

    最初の雑務

---
+++ RxSwiftSample/NewsApp/Controllers/NewsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  NewsViewController.swift
+//  RxSwiftSample
+//
+//  Created by 渕一真 on 2021/04/20.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+    
+}
--- RxSwiftSample/ViewController.swift
+++ RxSwiftSample/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func MVVMWithRxSwift(_ sender: UIButton) {
+        print("えもじ")
+        performSegue(withIdentifier: "toMVVM", sender: nil)
+    }
 }
 
