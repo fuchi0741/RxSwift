387ce79b7d5759b108e6e4da3419a0d96e9e0a75
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-05-03 18:39:06 +0900

    サンプルコード追加

--- RxSample.playground/Contents.swift
+++ RxSample.playground/Contents.swift
@@ -4,21 +4,53 @@ import RxCocoa
 
 let disposeBug = DisposeBag()
 
-print("(1)--------------------")
-let subject = PublishSubject<String>()
-subject.onNext("テキスト")
-subject.onNext("テキスト2")
+print("(1)PublishSubject")
+let publishSubject = PublishSubject<String>()
+publishSubject.onNext("テキスト")
+publishSubject.onNext("テキスト2")
 
 
-subject.subscribe { event in
+publishSubject.subscribe { event in
     print(event)
 }
 
-subject.onNext("テキスト3")
-subject.onNext("テキスト4")
+publishSubject.onNext("テキスト3")
+publishSubject.onNext("テキスト4")
 
-subject.dispose()
+publishSubject.dispose()
 
-subject.onNext("テキスト5")
+publishSubject.onNext("テキスト5")
+
+print("(2)BehaviorSubject")
+let behaviorSubject = BehaviorSubject(value: "初期値")
+
+behaviorSubject.onNext("ラストテキスト")
+
+behaviorSubject.subscribe { event in
+    print(event)
+}
+
+print("(3)ReplaySubject")
+
+let replaySubject = ReplaySubject<String>.create(bufferSize: 2)
+
+replaySubject.onNext("テキスト1")
+replaySubject.onNext("テキスト2")
+replaySubject.onNext("テキスト3")
+
+print("1回目")
+replaySubject.subscribe {
+    print($0)
+}
+
+replaySubject.onNext("テキスト4")
+replaySubject.onNext("テキスト5")
+replaySubject.onNext("テキスト6")
+
+print("2回目")
+
+replaySubject.subscribe {
+    print($0)
+}
 
 print("EOL")
