ce2316631a623d27a6b5d2dce4b1a70a330bdbfd
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-05-08 03:34:50 +0900

    アプリ完成

--- RxSwiftSample/TODOApp/Controllers/AddToDoViewController.swift
+++ RxSwiftSample/TODOApp/Controllers/AddToDoViewController.swift
@@ -6,17 +6,27 @@
 //
 
 import UIKit
+import RxSwift
 
 final class AddToDoViewController: UIViewController {
     
+    private let taskSubject = PublishSubject<Task>()
     
-    @IBOutlet weak var prioritySegementdControl: UISegmentedControl!
-    
-    @IBOutlet weak var todoTextField: UITextField!
-    
-    
-    
-    @IBAction func didTapSaveButton(_ sender: UIButton) {
+    var taskSubjectObservable: Observable<Task> {
+        return taskSubject.asObservable()
     }
     
+    @IBOutlet weak var prioritySegementdControl: UISegmentedControl!
+    @IBOutlet weak var todoTextField: UITextField!
+    
+    @IBAction func didTapSaveButton(_ sender: UIButton) {
+        
+        guard let priority = Priority(rawValue: self.prioritySegementdControl.selectedSegmentIndex),
+              let title = self.todoTextField.text else { return }
+        
+        let task = Task(title: title, priority: priority)
+        taskSubject.onNext(task)
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
--- RxSwiftSample/TODOApp/Controllers/TodoListViewController.swift
+++ RxSwiftSample/TODOApp/Controllers/TodoListViewController.swift
@@ -6,14 +6,81 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class TodoListViewController: UIViewController {
     
-    @IBOutlet weak var priorySegementedControl: UISegmentedControl!
-    @IBOutlet weak var tableView: UITableView!
+    private let disposeBag = DisposeBag()
+    private var tasks = BehaviorRelay<[Task]>(value: [])
+    private var filteredTasks = [Task]()
     
+    @IBOutlet weak private var priorySegementedControl: UISegmentedControl!
     
-    @IBAction func didTapPrityValueChanged(_ sender: UISegmentedControl) {
+    @IBOutlet weak private var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            let nib = UINib(nibName: "TodoListTableViewCell", bundle: nil)
+            tableView.register(nib, forCellReuseIdentifier: "TodoListTableViewCell")
+        }
     }
     
+    @IBAction private func didTapPrityValueChanged(_ sender: UISegmentedControl) {
+        let AllSegementedItem = 1
+        let priority = Priority(rawValue: sender.selectedSegmentIndex - AllSegementedItem)
+        filterTasks(by: priority)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let vc = segue.destination as? AddToDoViewController else { return }
+
+        
+        //監視する対象を見る
+        vc.taskSubjectObservable.subscribe(onNext: { [weak self] task in
+            guard let self = self else { return }
+            
+            let AllSegementedItem = 1
+            let priority = Priority(rawValue: self.priorySegementedControl.selectedSegmentIndex - AllSegementedItem)
+            
+            var existingTasks = self.tasks.value
+            existingTasks.append(task)
+            
+            self.tasks.accept(existingTasks)
+            
+            self.filterTasks(by: priority)
+        }).disposed(by: disposeBag)
+    }
+    
+    private func updateTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    private func filterTasks(by priority: Priority?) {
+        if priority == nil {
+            self.filteredTasks = self.tasks.value
+            self.updateTableView()
+        } else {
+            self.tasks.map { tasks in
+                return tasks.filter { $0.priority == priority! }
+            }.subscribe(onNext: { [weak self] tasks in
+                guard let self = self else { return }
+                self.filteredTasks = tasks
+                self.updateTableView()
+            }).disposed(by: disposeBag)
+        }
+    }
+}
+
+extension TodoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredTasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListTableViewCell", for: indexPath) as? TodoListTableViewCell else { return UITableViewCell() }
+        cell.textLabel?.text = filteredTasks[indexPath.row].title
+        return cell
+    }
 }
